e62289ad18aa8542b837d0d12e21a447f25a14c4
backmeupplz <backmeupplz@gmail.com>
2019-01-12 16:20:35 -0800

    finish functionality

--- src/helpers/inline.ts
+++ src/helpers/inline.ts
@@ -5,11 +5,11 @@ const fuzzysearch = require('fuzzysearch')
 export function setupInline(bot: Telegraf<ContextMessageUpdate>) {
   bot.on('inline_query', async ({ inlineQuery, answerInlineQuery, dbuser }) => {
     const offset = parseInt(inlineQuery.offset) || 0
-    const templates = dbuser.templates
-      .filter(t =>
-        inlineQuery.query ? fuzzysearch(inlineQuery.query, t.text) : true
-      )
-      .splice(offset, 30)
+    let templates = dbuser.templates
+    if (inlineQuery.query) {
+      templates = templates.filter(t => fuzzysearch(inlineQuery.query, t.text))
+    }
+    templates = templates.splice(offset, 30)
     const results = templates.map(template => ({
       type: 'article',
       id: template.name,
