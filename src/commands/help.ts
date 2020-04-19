490f8af8da323fd91793c848336935be267a206d
backmeupplz <backmeupplz@gmail.com>
2020-04-19 09:54:08 -0700

    add todorant

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,7 +2,9 @@
 import { Telegraf, ContextMessageUpdate } from 'telegraf'
 
 export function setupHelp(bot: Telegraf<ContextMessageUpdate>) {
-  bot.command(['help', 'start'], ctx => {
-    ctx.replyWithHTML(ctx.i18n.t('help'))
+  bot.command(['help', 'start'], (ctx) => {
+    ctx.replyWithHTML(ctx.i18n.t('help'), {
+      disable_web_page_preview: true,
+    })
   })
 }
